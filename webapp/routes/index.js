bdd95520d7ee561dce17bafa25f0aad53b5c665b
hassaanp <pasha.hassaan@gmail.com>
2015-02-23 17:36:18 +0500

    Update index.js

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
-
+router.get('/done', function(req, res) {
+  res.render('done');
+});
 module.exports = router;
